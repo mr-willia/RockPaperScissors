305ac2b7a4283a38d2d41986f5937d6977ab239c
mr-willia <liwilliam2419@gmail.com>
2022-11-24 17:52:38 -0800

    modify myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,31 @@
-var myArray = ['Rock', 'Paper', 'Scissors'];
+var myArray = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
     return myArray[Math.floor(Math.random() * myArray.length)];
 }
 console.log(getComputerChoice());
+
+
+
+function playRound(playerSelection, computerSelection) {
+    
+    if (playerSelection === computerSelection) {
+        return "It's a Tie!";
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You Lose! Paper beats Rock"; 
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You Win! Scissors beats Paper"; 
+    }
+} 
+input = prompt("Enter your Choice!");
+
+const computerSelection = getComputerChoice();
+console.log(playRound(input, computerSelection));
